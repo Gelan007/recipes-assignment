27df9769c9330aacaeaa26c6078679884eafc2e0
Anatolii_Filipenko <anatoliy.fil033@gmail.com>
2024-08-21 22:56:14 +0300

    fixed card layout

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -14,10 +14,12 @@ const Card: React.FC<CardProps> = (props) => {
     return (
         <div className={s.card}>
             <div className={s.photo}><img src={props.image || defaultImage} alt="avatar"/></div>
-            <div className={s.name}>{props.name}</div>
-            <div className={s.contacts}>
-                <div className={s.contacts__role}>Категорія: {props.category}</div>
-                <div className={s.contacts__email}>Місце походження: {props.area}</div>
+            <div className={s.card__content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.description}>
+                    <div className={s.description__category}>Категорія: <span>{props.category}</span></div>
+                    <div className={s.description__area}>Місце походження: <span>{props.area}</span></div>
+                </div>
             </div>
         </div>
     );
